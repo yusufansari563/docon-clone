9f066729ccb423be7903b844fab2232df5163b60
yusufansari <yusuf.ansari@crossasyst.com>
2023-04-11 02:00:31 +0530

    yusuf branch created

--- src/presentation/navigation/Stack.tsx
+++ src/presentation/navigation/Stack.tsx
@@ -1,11 +1,12 @@
 import { createStackNavigator } from "@react-navigation/stack";
 import { Details } from "../screens/Details/Details";
+import BookAppointment from "../screens/BookAppointment";
 
 const Stack = createStackNavigator();
 export function TabAScreen() {
   return (
     <Stack.Navigator>
-      {/*<Stack.Screen name='TabA Details' component={Details} />*/}
+      <Stack.Screen name='Book an Appointment' component={BookAppointment} />
     </Stack.Navigator>
   );
 }
---
+++ src/presentation/screens/BookAppointment/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+import { Avatar } from "native-base";
+
+export default function BookAppointment() {
+    return (
+        <View>
+
+        </View>
+    )
+}
