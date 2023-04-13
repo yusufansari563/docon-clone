602e90ef8277c52ca1a9488b919a2d6079478eda
Ajit-28 <palajit28@gmail.com>
2023-04-13 10:58:20 +0530

    "slides completed"

--- src/presentation/components/Carousel/Carousel.tsx
+++ src/presentation/components/Carousel/Carousel.tsx
@@ -1,12 +1,10 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-import CarouselText from './CarouselText'
 import CarouselSlides from './CarouselSlides'
 
 export default function Carousel() {
   return (
     <View>
-        <CarouselText />
         <CarouselSlides />
     </View>
   )
--- src/presentation/screens/Home/Home.tsx
+++ src/presentation/screens/Home/Home.tsx
@@ -1,10 +1,25 @@
-import {Text, View} from 'react-native'
 import React from 'react'
+import { View, Text, Image } from 'react-native';
+import SearchBar from '../../components/SearchBar/SearchBar';
+import Carousel from '../../components/Carousel/Carousel';
 
 export default function Home() {
     return (
-        <View>
-            <Text>Home</Text>
+        <View className=' bg-white flex flex-1 w-fit'>
+          <View className='grid grid-rows-4 '>
+              <View className=' flex-row   mx-5 mt-5'>
+                <View className='basis-1/1 mr-5'>
+                  <Text className='text-2xl font-bold'>Find Your Doctor</Text>
+                  <Text className='text-base mt-1'>Book an appointmant for consultation</Text>
+                </View>
+
+                <View className='basis-1/2 m-auto  items-center'>
+                  <Image className='h-20 w-16 ' source={require('../../Assets/images/R.jpg')} />
+                </View>
+              </View>
+            </View>
+          <SearchBar />
+          <Carousel />
         </View>
     )
 }
