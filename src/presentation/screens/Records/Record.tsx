6926db7e328efc70060b0328fe08580bb88e15cf
Ajit-28 <palajit28@gmail.com>
2023-04-17 15:14:09 +0530

    "adding records screens"

--- src/presentation/screens/Records/Record.tsx
+++ src/presentation/screens/Records/Record.tsx
@@ -1,10 +1,28 @@
-import {Text, View} from 'react-native'
+import { Text, View, Image } from 'react-native'
+
 import React from 'react'
 
+import { Button } from 'native-base'
+
 export default function Record() {
+
     return (
-        <View>
-            <Text>Record</Text>
-        </View>
+
+        <>
+
+            <View className='h-full flex items-center justify-center'>
+
+                <Image className=' h-20 w-20 mb-4 ' source={require('../../Assets/images/FolderAdd.png')} />
+                <Text className='text-xl font-bold mb-5'>No Patient History</Text>
+                <Text className='text-base mb-6'>Please book your first appointment of Video or Physical consultationas per your convenience</Text>
+                <Button className='bg-blue-500 rounded-none'>Book Now</Button>
+
+            </View>
+
+
+
+        </>
+
     )
+
 }
